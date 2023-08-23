032cafd4619b2d599c2a02d38a8a095993092895
mmarew@gmail.com <mmarew@gmail.com>
2023-08-23 10:06:07 +0300

    updates

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
-const userRouter = require("./server/api/users/user.router"); 
+const userRouter = require("./api/users/user.router.js");
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
